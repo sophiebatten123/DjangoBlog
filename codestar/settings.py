016c7e944a5a0d8a94d393ff2c3b604bbae9d300
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2022-02-03 11:15:14 +0000

    Allowed hosts has been changed

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022-sophie.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['codestar2022-sophie.herokuapp.com', 'localhost']
 
 
 # Application definition
